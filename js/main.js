0d0a608dfea5b7bf74a2b678c2f037aac471fedb
SqweryYT <135578052+SqweryYT@users.noreply.github.com>
2023-06-05 10:20:32 +0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let arr = ["davlat","amir","laziz","shoxruh","muhamadali"]
+
+let name = prompt('Первая буква имени')
+
+let finded = arr.find(item => item[0] === name)
+
+console.log(finded);
+
+confirm('Вы Действительно Хотите Удалить Это имя?')
+
+arr.slice()
